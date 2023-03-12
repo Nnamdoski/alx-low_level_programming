d1db74c893ce379ec03eec2b858508b828d170b1
Nnamdoski <jaknnamdoski@gmail.com>
2023-03-11 20:01:24 -0800

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,8 +12,9 @@ int main(int argc, char *argv[])
 {
 	int num, j, result;
 	int coins[] = {25, 10, 5, 2, 1};
+
 	if (argc != 2)
-	{
+
 		printf("Error\n");
 		return (1);
 	}
@@ -21,6 +22,7 @@ int main(int argc, char *argv[])
 	num = atoi(argv[1]);
 	result = 0;
 	if (num < 0)
+
 	{
 		printf("0\n");
 		return (0);
