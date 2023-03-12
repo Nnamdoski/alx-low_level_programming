9db47d308818c916ef662ce0ccc85ead18a52966
Nnamdoski <jaknnamdoski@gmail.com>
2023-03-11 19:33:03 -0800

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,13 +16,11 @@
 		count = 0;
 		while (count < strlen(str)) /*count string*/
 
-
 		{
 			if (!isdigit(str[count])) /*check if str there are digit*/
 			{
 				return (0);
 			}
-
 
 			count++;
 		}
